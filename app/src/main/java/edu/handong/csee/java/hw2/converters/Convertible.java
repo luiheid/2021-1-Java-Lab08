1ac7e370f1fb921dd8f69afe7e1b3bb48164f308
luiheid <hellolisa99@naver.com>
2021-05-06 20:30:54 +0900

    Convertible.java is committed

---
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/Convertible.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.java.hw2.converters;
+
+/**This is interface */
+public interface Convertible {
+
+    /** THis is setfromvalue*/
+    public void setFromValue(double fromValue);
+    /** THis is getconvertedvalue*/
+    public double getConvertedValue();
+    /** THis is convert*/
+    public void convert();
+
+}
